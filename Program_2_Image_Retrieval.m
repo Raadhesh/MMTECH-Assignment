907c624c7706da732d9a09e64d53a3eaa2477e15
Raadhesh <35178748+Raadhesh@users.noreply.github.com>
2019-09-12 21:58:35 +0800

    Add files via upload

--- Program_2_Image_Retrieval.m
+++ Program_2_Image_Retrieval.m
@@ -12,8 +12,8 @@ addpath(pwd);
 load('new_database_cbir.mat','database');% loading the database mat file
 prompt = 'Enter image index in database , e.g 1, 101 >> ';
 x = input(prompt) ;
-prompt = 'Enter image label in database , e.g 1 for beach and 2 for building >> ';
-label_true = input(prompt) ;
+% prompt = 'Enter image label in database , e.g 1 for beach and 2 for building >> ';
+% label_true = input(prompt) ;
 prompt = 'Enter number of images to be retrieved >> ';
 num_images = input(prompt) ;
 prompt = 'RGB results 1 for true and 0 for false >> ';
@@ -22,6 +22,10 @@ prompt = 'HSV results 1 for true and 0 for false >> ';
 bool_HSV = input(prompt) ;
 prompt = 'CNN results 1 for true and 0 for false >> ';
 bool_CNN = input(prompt) ;
+prompt = 'MSE results 1 for true and 0 for false >> ';
+bool_MSE = input(prompt) ;
+prompt = 'SSIM results 1 for true and 0 for false >> ';
+bool_SSIM = input(prompt) ;
 
 n=1;
 
@@ -42,30 +46,52 @@ numIm = length(database)  ; % this give the number of images in database
 distRGB = (0);
 distHSV = (0);
 distCNN = (0);
-
+distMSE = (0);
+distSSIM = (0);
+tic
 for i=1:numIm
     distRGB(i) = getEuclideanDistance(HistQRGB, database(i).featRGB);
     distHSV(i) = getEuclideanDistance(HistQHSV, database(i).featHSV);
     distCNN(i) = getEuclideanDistance(HistQCNN, database(i).featCNN);
+    I = database(i).imageName ;
+    if bool_MSE == 1
+        distMSE(i) = getMSE(imfile, I);
+    end
+    if bool_SSIM == 1
+        distSSIM(i) = getSSIM(imfile, I);
+    end
+    fprintf('Difference between query image and database image %d\n', i);
 end
-
+toc
 %% Sort dist in ascending order
 
 [sorted_distRGB, idxRGB] = sort(distRGB);
 [sorted_distHSV, idxHSV] = sort(distHSV);
 [sorted_distCNN, idxCNN] = sort(distCNN);
+[sorted_distMSE, idxMSE] = sort(distMSE);
+[sorted_distSSIM, idxSSIM] = sort(distSSIM, 'descend');
 
 
 %% USER wants to show 30 images
-id_listRGB = idxRGB(1:num_images) ;
-id_listHSV = idxHSV(1:num_images) ;
-id_listCNN = idxCNN(1:num_images) ;
+label_true = database(idxRGB(1)).label;
+id_listRGB = idxRGB(2:(num_images+1)) ;
+id_listHSV = idxHSV(2:(num_images+1)) ;
+id_listCNN = idxCNN(2:(num_images+1)) ;
+if (bool_MSE == 1) || (bool_SSIM == 1)
+    id_listMSE = idxMSE(2:(num_images+1)) ;
+    id_listSSIM = idxSSIM(2:(num_images+1)) ;
+end
 count_trueRGB = 0;
 count_falseRGB = 0;
 count_trueHSV = 0;
 count_falseHSV = 0;
 count_trueCNN = 0;
 count_falseCNN = 0;
+count_trueMSE = 0;
+count_falseMSE = 0;
+count_trueSSIM = 0;
+count_falseSSIM = 0;
+
 l = sqrt(num_images);
 l = round(l);
 b = 0;
@@ -86,9 +112,11 @@ if bool_RGB == 1
         else
             count_falseRGB = count_falseRGB +1;
         end
-
+        
         strRGB = sprintf('%d',labelRGB);  strRGB =[ '  Label = ' strRGB] ;
-        subplot(l,b,j) , imshow(imfileRGB) , title(strRGB) ;
+        strRGB2 = sprintf('%.2f', sorted_distRGB(j)); strRGB2=['  Disimmilarity = ' strRGB2];
+        strRGB = strRGB + "\n" + strRGB2;
+        subplot(l,b,j) , imshow(imfileRGB) , title(compose(strRGB)) ;
 
     end
     resultRGB = count_trueRGB/(count_trueRGB+count_falseRGB);
@@ -111,7 +139,9 @@ if bool_HSV == 1
 
 
         strHSV = sprintf('%d',labelHSV);  strHSV =[ '  Label = ' strHSV] ;
-        subplot(l,b,j) , imshow(imfileHSV) , title(strHSV) ;
+        strHSV2 = sprintf('%.2f', sorted_distHSV(j)); strHSV2=['  Disimmilarity = ' strHSV2];
+        strHSV = strHSV + "\n" + strHSV2;
+        subplot(l,b,j) , imshow(imfileHSV) , title(compose(strHSV)) ;
 
     end
     resultHSV = count_trueHSV/(count_trueHSV+count_falseHSV);
@@ -135,11 +165,65 @@ if bool_CNN == 1
 
 
         strCNN = sprintf('%d',labelCNN);  strCNN =[ '  Label = ' strCNN] ;
-        subplot(l,b,j) , imshow(imfileCNN) , title(strCNN) ;
+        strCNN2 = sprintf('%.2f', sorted_distCNN(j)); strCNN2=['  Disimmilarity = ' strCNN2];
+        strCNN = strCNN + "\n" + strCNN2;
+        subplot(l,b,j) , imshow(imfileCNN) , title(compose(strCNN)) ;
 
     end
     resultCNN = count_trueCNN/(count_trueCNN+count_falseCNN);
-    fprintf('\nCNN probability is %f \n', resultRGB);
+    fprintf('\nCNN probability is %f \n', resultCNN);
+end
+
+if bool_MSE == 1
+    figure('Name', 'MSE','NumberTitle','off');
+    for j=1:num_images
+
+        idMSE = id_listMSE(j) ;
+
+        imfileMSE = database(id_listMSE(j)).imageName ;
+        labelMSE = database(idMSE).label;
+        
+        if label_true == labelMSE
+            count_trueMSE = count_trueMSE + 1;
+        else
+            count_falseMSE = count_falseMSE +1;
+        end
+
+
+        strMSE = sprintf('%d',labelMSE);  strMSE =[ '  Label = ' strMSE] ;
+        strMSE2 = sprintf('%.2f', sorted_distMSE(j)); strMSE2=['  Disimmilarity = ' strMSE2];
+        strMSE = strMSE + "\n" + strMSE2;
+        subplot(l,b,j) , imshow(imfileMSE) , title(compose(strMSE)) ;
+    end
+    resultMSE = count_trueMSE/(count_trueMSE+count_falseMSE);
+    fprintf('\nMSE probability is %f \n', resultMSE);
+end
+
+
+if bool_SSIM == 1
+    figure('Name', 'SSIM','NumberTitle','off');
+    for j=1:num_images
+
+        idSSIM = id_listSSIM(j) ;
+
+        imfileSSIM = database(id_listSSIM(j)).imageName ;
+        labelSSIM = database(idSSIM).label;
+        
+        if label_true == labelSSIM
+            count_trueSSIM = count_trueSSIM + 1;
+        else
+            count_falseSSIM = count_falseSSIM +1;
+        end
+
+
+        strSSIM = sprintf('%d',labelSSIM);  strSSIM =[ '  Label = ' strSSIM] ;
+        strSSIM2 = sprintf('%.2f', sorted_distSSIM(j)); strSSIM2=['  Simmilarity = ' strSSIM2];
+        strSSIM = strSSIM + "\n" + strSSIM2;
+        subplot(l,b,j) , imshow(imfileSSIM) , title(compose(strSSIM)) ;
+
+    end
+    resultSSIM = count_trueSSIM/(count_trueSSIM+count_falseSSIM);
+    fprintf('\nSSIM probability is %f \n', resultSSIM);
 end
 
 
